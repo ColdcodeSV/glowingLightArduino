38645cedcb73e9cc2937174b4ef788b612a54d2f
Nazeh85 <160012102+Nazeh85@users.noreply.github.com>
2024-03-17 12:41:03 +0100

    Addresses of the ports

---
+++ Task2/Labb2/Labb2.ino
@@ -0,0 +1,8 @@
+volatile uint8_t* ddrb = (volatile uint8_t*)0x24; // Data Direction Register för port B
+volatile uint8_t* portb = (volatile uint8_t*)0x25; // Port B register för att kontrollera utgångar
+
+volatile uint8_t* ddrc = (volatile uint8_t*)0x27; // Data Direction Register för port C
+volatile uint8_t* portc = (volatile uint8_t*)0x28; // Port C register för att kontrollera utgångar
+
+volatile uint8_t* ddrd = (volatile uint8_t*)0x2A; // Data Direction Register för port D
+volatile uint8_t* portd = (volatile uint8_t*)0x2B; // Port D register för att kontrollera utgångar
