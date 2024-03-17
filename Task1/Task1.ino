49f4d22950318c55103dfeb8e504914f20fc655c
Crazybane <112276387+ColdcodeSV@users.noreply.github.com>
2024-03-17 11:59:45 +0100

    added adresses to ports

---
+++ Task1/Task1.ino
@@ -0,0 +1,4 @@
+volatile uint8_t* ddrb  = (volatile uint8_t*)0x24;
+volatile uint8_t* portb = (volatile uint8_t*)0x25;
+volatile uint8_t* ddrd  = (volatile uint8_t*)0x2A;
+volatile uint8_t* portd = (volatile uint8_t*)0x2B;
