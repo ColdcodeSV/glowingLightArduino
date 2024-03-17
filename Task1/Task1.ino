055c9972b185ad9a0d76b2f44fe7a579be4fe3c2
Crazybane <112276387+ColdcodeSV@users.noreply.github.com>
2024-03-17 12:03:26 +0100

    created loop function to make the lights glow between 1-10s

--- Task1/Task1.ino
+++ Task1/Task1.ino
@@ -10,3 +10,24 @@ void setup() {
   // Set pin 2 (PD2) as an output for controlling LED
   *ddrd |= (1 << 2);
 }
+
+void loop() {
+  // Toggle pin 2 (PD2) to control the state of PB5 (pin 13)
+  *portd ^= (1 << 2); // Toggle PD2
+  
+  // Read the state of pin 2 (PD2)
+  int controlState = (*portd >> 2) & 1;
+  
+  // Set pin 12 (PB5) based on the state of pin 2 (PD2)
+  if (controlState == 1) {
+    *portb |= (1 << 5); // Turn on PB5
+  } else {
+    *portb &= ~(1 << 5); // Turn off PB5
+  }
+  
+  // Generate a random delay between 1 and 10 seconds for controlling LED
+  int delayTime = random(1000, 10001); // Random delay between 1 and 10 seconds
+  delay(delayTime);
+  
+  delay(1000); // Wait for a second
+}
